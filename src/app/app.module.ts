10f4577a91dd8d6eda2f3ce395f54f3c3c5a98b3
Russell Anderson <russellanderson2000@gmail.com>
2020-09-08 12:35:11 -0400

    routes and components are a mess, need to fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
